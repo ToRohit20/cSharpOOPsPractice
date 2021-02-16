e298b115219ab5cb6ed965622b694fef9c1feed3
rohitsingh.bcet <rohitsingh.bcet@outlook.com>
2021-02-16 23:55:43 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPsConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to .Net core console Application!");
+        }
+    }
+}
